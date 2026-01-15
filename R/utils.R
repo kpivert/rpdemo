0e9533489447e8b368f9de6f4c0897966d01c678
Kurtis Pivert <kurtis.pivert@gmail.com>
2026-01-15 12:47:27 -0500

    Add new arguments

--- R/utils.R
+++ R/utils.R
@@ -4,14 +4,18 @@
 #'
 #' @param x a numeric vector of counts
 #' @param threshold numeric thresholds for the number below which to suppress
-#'
+#' @param symbol What the suppressed values should be, default is NA
 #' @returns A vector with low counts suppressed
 #'
 #' @export
 #' @examples
 #' suppress_counts(1:10)
 #' suppress_counts(1:20, threshold= 20)
-suppress_counts <- function(x, threshold = 5) {
-  x[x < threshold] <- NA
+#' suppress_counts(1:20, symbol = "*")
+suppress_counts <- function(x, threshold = 5, symbol = NA) {
+  if (!is.numeric(x)) {
+    stop("Input x must be numeric.")
+  }
+  x[x < threshold] <- symbol
   return(x)
 }
